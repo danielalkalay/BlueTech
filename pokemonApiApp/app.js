61e96f4004bf9845b3189a9264772a401ee518e3
danielalkalay <danielalkalay4@gmail.com>
2023-08-03 17:42:41 +0300

    interval + timeout pokemon fetch

--- pokemonApiApp/app.js
+++ pokemonApiApp/app.js
@@ -1,36 +1,20 @@
-// DOM:
+// fetch from api + closure + auto init function:
+
+let init = `https://pokeapi.co/api/v2/pokemon/1/`;
+
 const catchThemAll = document.createElement("button");
 let title = document.querySelector(".title");
 
-catchThemAll.innerText = "new pokemon";
-document.body.appendChild(catchThemAll);
+// DOM:
 let pic = document.createElement("img");
+catchThemAll.innerText = "roll new pokemon";
+// func:
+document.body.appendChild(catchThemAll);
 document.body.appendChild(pic);
 
-// vars:
+// start point:
+// fetch the start pokemon (default by hard code)
 
-let init = "https://pokeapi.co/api/v2/pokemon/1/";
-
-// fetch from api + closure + auto init function:
-
-const getPokemon = () => {
-  console.log("waiting");
-  let randomNumber = Math.ceil(Math.random() * 1010);
-  setTimeout(function () {
-    console.log("after waiting for 5 sec");
-    fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`)
-      .then((res) => res.json())
-      .then((data) => {
-        title = data.name;
-        pic.src = data.sprites.other.home.front_default;
-        title.innerText = data.name;
-        console.log(` ${data.name} is our random pokemon of the day`);
-        console.log(data);
-        console.log(pic);
-      });
-  }, 5000);
-};
-catchThemAll.addEventListener("click", getPokemon);
 const fetchData = ((init) => {
   let fetchData = init;
   console.log(`This is the first init ${fetchData} `);
@@ -45,6 +29,57 @@ const fetchData = ((init) => {
     });
 
   return () => {
-    getPokemon;
+    pokeInterval;
   };
 })(init);
+
+//  creating an interval that runs every 4 seconds and getting
+//  a pokemon name untill gettin to four pokemons
+
+let count = 0;
+let interID;
+
+const pokeInterval = () => {
+  interID = setInterval(myInterval, 4000);
+};
+
+const myInterval = () => {
+  console.log("waiting");
+  count++;
+  console.log(count);
+  getPokemon();
+  // console.log(data.name);
+  if (count === 4) {
+    // stopInterval();
+    console.log("four pokemons");
+    stopInterval();
+  }
+};
+
+const stopInterval = () => {
+  clearInterval(interID);
+  interID = null;
+};
+
+catchThemAll.addEventListener("click", pokeInterval);
+
+const getPokemon = () => {
+  console.log("get pokemon");
+
+  setTimeout(function () {
+    console.log("after waiting for few sec");
+    let randomNumber = Math.ceil(Math.random() * 1010);
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}/`)
+      .then((res) => res.json())
+      .then((data) => {
+        title = data.name;
+        pic.src = data.sprites.other.home.front_default;
+        // title.innerText = data.name;
+        console.log(` ${data.name} is our random pokemon of the day`);
+        // console.log(data);
+        // console.log(pic);
+      })
+      .catch((error) => console.error(error));
+  }, 2000);
+};
